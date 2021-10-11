e30e5dba2143d4f0bbbb260d1ab56f3a71893f3a
loicvdk <loic.vanderkelen@gmail.com>
2021-10-11 16:19:56 +0200

    first request

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Python and R for Data Science.
+
+## First day
+
+- Virtual Env created
+- Connected to the API 
+- Did first request 
+
+## To Do
+- Wait 24h to reset the 1000 request limit 
+- Populate a json/csv file
--- api_requests.py
+++ api_requests.py
@@ -1,1 +1,12 @@
-# Initial file
+# Initial imports
+import requests
+
+# Constant objects
+API_KEY = "9d167066"
+API_ADDRESS = f"http://www.omdbapi.com/?apikey={API_KEY}&"
+INITIAL_IMDB_ID = "tt0000001"
+
+# Requests
+response = requests.get(API_ADDRESS + "i=" + INITIAL_IMDB_ID)
+print(response)
+print(response.json())
