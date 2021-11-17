55d3628c2da44848c2fcc32b572a8e5dd05a5632
hvenance <hadrien.venance@studenti.luiss.it>
2021-11-17 14:13:15 +0100

    latest udpate

---
+++ Project_Dataset2.R
@@ -0,0 +1,214 @@
+rm(list=ls())
+
+#install and load libraries
+packages = c("dplyr", "ggplot2", "ggmap", "sf", "mapview", "corrplot", "plotly", "lubridate")
+
+# Install packages not yet installed
+installed_packages = packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+#packages loading
+lapply(packages, library, character.only = TRUE)
+
+
+#read csv file
+data <- read.csv("C:/Users/Hadrien Venance/python_and_r_luiss_2021/python_and_r_luiss_2021/Dataset2_OpenMeteo.txt")
+
+View(data)
+head(data)
+summary(data)
+
+#remove all the columns containing unit measures
+data <- data %>% 
+    select(-contains("units"))
+
+#rename the columns, without the hourly and other useless punctuation
+colnames(data)<-gsub("hourly","",colnames(data))
+
+data <- data %>%
+  rename_with(~ gsub('[[:punct:]]', '', .x))
+
+head(data)
+
+#transform the lat and long coordinates into cities location
+locations<- st_as_sf(data, coords = c("longitude", "latitude"), crs = 4326)
+mapview(locations)
+#in this df we now also have a unique identifier for the variables
+
+unique(data$elevation)
+unique(locations$geometry)
+data[505,] == data[1,]
+#unlike what we read in the README file it seems that only 5 cities data have been uploaded.
+#as such, Rome is missing in the dataset; the issue is that the content of what was supposed to be Rome only contains info related to the 1st city 
+#which is Berlin 
+
+#we decided then to remove the duplicates
+data %>% distinct()
+
+#converts in character type for ggplot object
+data$elevation <- as.character(data$elevation)
+
+ggplot(data) +
+  geom_point(mapping = aes(x = time, y= temperature2m, color = elevation))+
+  #scale_color_discrete(name = "City", labels = c("New York", "London", "Tokyo", "Berlin", "Paris"))+
+  ggtitle("Temperature in cities across time")+
+  theme(plot.title = element_text(hjust = 0.5))
+#on this plot it seems that elevation  is not really an indicator of the temperature
+#we observe no negative temperature and the maximum temperature is above 20 degrees
+
+
+names = c("New York", "London", "Tokyo", "Berlin", "Paris")
+elevation = sort(unique(data$elevation))
+dico = c()
+#populate the dico
+for (i in 1:5){
+  dico[elevation[i]] = names[i]
+}
+
+#group the observations per day to have a more readable x-axis
+data$day <- day(data$time)
+ggplot(data) +
+  geom_point(mapping = aes(x = day, y= temperature2m, color = elevation))+
+  scale_color_discrete(name = "City", labels = c("New York", "London", "Tokyo", "Berlin", "Paris"))+
+  ggtitle("Temperature in cities across days")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+#one of the advantages of using facet is that with categorical data we can focus on particular facets alone
+elevation.labs <- elevation
+names(elevation.labs) <- c("New York", "London", "Tokyo", "Berlin", "Paris")
+ggplot(data) +
+  geom_point(mapping = aes(x = day, y= temperature2m)) +
+  facet_wrap(~elevation, nrow = 2)+
+  ggtitle("Facet of temperature in elevation cities across days")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+
+#because the geom_boxplot line of code reads 'day' as numeric we have to edit the type of the variable
+data$day <- as.character(data$day)
+ggplot(data) +
+  geom_boxplot(mapping = aes(x = day, y= temperature2m, color=day))+
+  ggtitle("Boxplot of temperature in cities across days")+
+  theme(plot.title = element_text(hjust = 0.5))
+#on the day 3 it seems that there the most outliers
+
+
+#compute air temperature 2 meters above the ground
+temperature_by_cities <- data %>% 
+                      select(elevation, apparenttemperature) %>%
+                      group_by(elevation) %>% 
+                        summarise(temperature = mean(apparenttemperature, na.rm=TRUE))
+#rename the columns for visualisation purposes in ggplot
+names = c("New York", "London", "Tokyo", "Berlin", "Paris")
+temperature_by_cities$City_names <- names
+
+ggplot(temperature_by_cities)+
+  geom_point(mapping = aes(y = temperature, x= City_names , size = elevation))+
+  ggtitle("Temperature in cities displayed with their elevation")+
+  theme(plot.title = element_text(hjust = 0.5))
+#from this plot, we can rightfully inspect if there is a correlation between the temperature and the elevation of the cities
+
+temperature_by_cities$elevation <- as.numeric(temperature_by_cities$elevation)
+temperature_by_cities.cor <- cor(temperature_by_cities[,-3])
+corrplot(temperature_by_cities.cor, title="Correlation between cities elevation and temperature", mar=c(0,0,2,0))
+
+
+
+#in the database, we also noticed that there were 2 kind of temperatures, namely the air temperature 2 meters above the ground and the perceived
+#feel like temperature
+feels_temp_by_cities <- data %>% 
+  select(elevation, temperature2m, apparenttemperature, relativehumidity2m) %>%
+  group_by(elevation) %>% 
+  summarise(temperature = mean(temperature2m, na.rm=TRUE), feels_like_temp = mean(apparenttemperature, na.rm=TRUE), humidity = mean(apparenttemperature, na.rm=TRUE))
+feels_temp_by_cities$City_names <- names
+head(feels_temp_by_cities)
+
+#we moved the city names at the beginning of the dataframe for clarity purposes
+feels_temp_by_cities <- feels_temp_by_cities %>%
+    select(City_names, everything())
+head(feels_temp_by_cities)
+hottest_city <- which(feels_temp_by_cities$temperature == max(feels_temp_by_cities$temperature))
+hottest_city_name <- feels_temp_by_cities[hottest_city,1]
+hottest_city_name
+#from our analysis, it seems that Tokyo is by fat the hottest city in the dataset 
+
+figure <- with(feels_temp_by_cities, plot_ly(x=feels_like_temp, y=City_names, z=temperature, type="scatter3d", mode="markers", color=feels_like_temp))
+figure <- figure %>% add_markers()
+figure <- figure %>% layout(scene = list(xaxis = list(title = 'Apparent temperature'),
+                                     yaxis = list(title = 'City names'),
+                                     zaxis = list(title = 'Real temperature')))
+#as we can see from this 3D plot it seems that a higher apparent temperature is as expected related with a higher real measured temperature
+#to confirm our visual insights, let us compute the correlation
+feels_temp_by_cities$elevation <- as.numeric(feels_temp_by_cities$elevation)
+(feels_temp_by_cities.cor <- cor(feels_temp_by_cities[,-1]))
+corrplot(feels_temp_by_cities.cor, title = "Correlation between feels like temperature /n and supposedly correlated variables", mar=c(0,0,2,0))
+#correlations are really high between temperature and feels like temperature, as well as between temperatures and humidity 
+
+
+#look at humidity and precipitation
+water_by_cities <- data %>% 
+  select(elevation, precipitation, relativehumidity2m) %>%
+  group_by(elevation) %>% 
+  summarise(precipitation = mean(precipitation, na.rm=TRUE), humidity = mean(relativehumidity2m, na.rm=TRUE))
+
+#same renaming of columns as before 
+water_by_cities$City_names <- names
+
+ggplot(water_by_cities)+
+  geom_point(mapping = aes(y = precipitation, x= City_names , size = humidity))+
+  ggtitle("Precipitations in cities displayed with their elevation")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+water_by_cities$elevation <- as.numeric(water_by_cities$elevation)
+(water_by_cities.cor <- cor(water_by_cities[,-4]))
+corrplot(water_by_cities.cor, title = "Correlation between water related variables in cites and elevation", mar=c(0,0,2,0))
+#it seems that the elevation is slightly highly correlated with the humidity
+#surprisingly, the precipitation that happened the hour before the weather report does not seem correlated to humidity at all
+
+#winter/cold conditions
+unique(data[,9])
+#for all the selected cities, none of them has a single meter of snow on he ground
+
+
+#atmospheric pressure is an indicator of weather. As such, according to National Geographic,
+#When a low-pressure system moves into an area, it usually leads to cloudiness, wind, and precipitation. High-pressure systems usually lead to fair, calm weather. 
+#Let's try to verifiy this stated fact
+#(https://www.nationalgeographic.org/encyclopedia/atmospheric-pressure/#:~:text=Atmospheric%20pressure%20is%20an%20indicator,lead%20to%20fair%2C%20calm%20weather.)
+atmosphere_by_cities <- data %>% 
+  select(elevation, pressuremsl, relativehumidity2m, precipitation, temperature2m) %>%
+  group_by(elevation) %>% 
+  summarise(pressure = mean(pressuremsl, na.rm=TRUE), humidity = mean(relativehumidity2m, na.rm=TRUE), precipitation = mean(precipitation, na.rm=TRUE), temperature = mean(temperature2m, na.rm=TRUE))
+
+#same renaming of columns 
+atmosphere_by_cities$City_names <- names
+head(atmosphere_by_cities)
+
+atmosphere_by_cities <- atmosphere_by_cities%>% 
+   select(City_names, everything())
+
+ggplot(atmosphere_by_cities)+
+  geom_point(mapping = aes(y = precipitation, x= pressure , size = humidity))+
+  ggtitle("Precipitations by pressure levels displayed with their humidity level")+
+  theme(plot.title = element_text(hjust = 0.5))
+#contrarily to what we expected from national geographic, a higher pressure does not seem to indicate higher precipitation
+
+atmosphere_by_cities$elevation <- as.numeric(atmosphere_by_cities$elevation)
+(atmosphere_by_cities.cor <- cor(atmosphere_by_cities[,-1]))
+corrplot(atmosphere_by_cities.cor, title = "Correlation between atmosphere and water related variables", mar=c(0,0,2,0))
+#it looks like there is a strong negative correlation between pressure and humidity and precipitation
+
+#let us now considerate the relation between the freezing level height and the atmospheric pressure
+freezing_by_cities <- data %>% 
+  select(elevation, freezinglevelheight ,pressuremsl, relativehumidity2m, precipitation, temperature2m) %>%
+  group_by(elevation) %>% 
+  summarise(freezing_height = mean(freezinglevelheight, na.rm= T), pressure = mean(pressuremsl, na.rm=TRUE), humidity = mean(relativehumidity2m, na.rm=TRUE), precipitation = mean(precipitation, na.rm=TRUE), temperature = mean(temperature2m, na.rm=TRUE))
+
+ggplot(freezing_by_cities)+
+  geom_point(mapping = aes(y = freezing_height, x= pressure , color = elevation))+
+  ggtitle("Freezing height by pressure levels displayed with their elevation level")+
+  theme(plot.title = element_text(hjust = 0.5))
+#it seems that freezing height is not really related to pressure 
+
+
+
---
+++ README.md
@@ -0,0 +1,106 @@
+# Python and R for Data Science.
+
+#Task 1. Data collection task
+
+For the first part of the assignment we have been asked to create a 1000 rows database using an API. We chose the OMDB API. To create the database (output_db.json and output_db.csv), we used the following functions: 
+
+## Functions
+
+-  ```generate_1000_ids()```: this function takes no positional arguments and returns a list of 1000 film ids. Thanks to the OMDB documentation we knew that the ids are composed as follows:  every id starts with 'tt' and followed by 7 digits (e.g. the first film as the id tt0000001). To create those ids we first populated an array containing only the digital part, ```suffix_list```. The first element is 0000001 and the last one is 0001000. Then we populated a second array, ```ids_list``` containing the prefix (i.e. tt) followed by the suffix created before. 
+
+- ```populate_dataframe()```: this function takes one positional argument: a list of ids and returns a pandas dataframe. First, we initialize an empty pandas dataframe called ```df```. Second, we loop through the list of ids and for each id we send a request to the server (the OMDB API). We store the response in a variable ```response``` and transform it into a json object with the ```.json()``` function. Then we add the response into our dataframe. Finally, we return this dataframe.  
+
+- ```create_database()```: this function takes no positional arguments and output a _json file_. First, thanks to the two previous functions, we create a ```data``` variable containing our 1000 rows dataframe. Then, we export it as a json and a csv file. Finally, we print 'Completed!' to show the user that everythings succeeded.     
+
+
+We wrapped the ```created_database()``` function in a ```if __name__ == "__main__":``` in order to be able to export each individual function, if needed, without creating a database everytime. 
+
+## Variable
+For this project we needed to instanciate 3 variables. Variables name are written with capital letter to respect pep 8 coding convention (constant variables must be name with capital letters). 
+- ```API_KEY```: this is the personnal API key received from the OMDB website. 
+- ```API_ADDRESS```: this variable store the url needed to connect to the API endpoint.
+- ```INITIAL_IMDB_ID_PREFIX```: this variable store ids' prefix (i.e. 'tt').
+
+## Imports 
+- ```import requests```: we imported the requests library to communicate with the OMDB API. We used mainly the ```.get()``` method. 
+- ```import pandas as pd```: we imported the pandas library to handle the data retrieved from the API. Pandas allowed us to store the data in a DataFrame which made it easy to query. It also allowed us to export the database as json and csv files.  
+
+## Virtual Environment
+To make the collaboration between group member easier, we decided to create a python virtual environment, ```venv```. This allowed us to work with the same environment on each machine (same packages, same version, etc.). 
+
+#Task 2. Data Analysis
+
+##Dataset-1
+
+
+##Dataset-2
+
+This dataset was retrieved on the GitHub repository available at this link: ```https://github.com/enricoromano/Python-and-R-project-```.
+It encompasses weather data accessed through the API [OpenMeteo](https://open-meteo.com/en/docs).
+In total, 
+
+###Imports
+Various libraries have been used:
+* ```library(dplyr)```: this library was used to manipulate the dataset (i.e. select particular variables, create new dataframe subsets,etc.)
+* ```library(ggplot2)```: ggplot2 is one of the most used tool for visualisation purposes in R.
+* ```library(ggmap)```: it is a library to visualize spatial data on static maps. It was used to visualise on a real map to which cities the latitude and longitude coordinates matched.
+* ```library(sf)```: this library allows to transform an object into a simple features which have emphasis on spatial geometry and coordinates.
+* ```library(mapview)```: it provides functions to very quickly and conveniently create interactive visualisations of spatial data.
+* ```library(corrplot)```: corrplot provides a visual exploratory tool on correlation matrix that helps detect hidden patterns among variables.
+* ```library(plotly)```: it supports the functions to create interactive graphs. Thanks to this library a 3D-plot was displayed for the apparent temperature, the actual one and the cities.
+* ```library(lubridate)```: it eases the handling of datatimes. 
+
+###Data cleaning
+Initially, the dataset was made of 1008 observations and 25 variables.
+However, the columns containing the unit values were **removed** as they are not useful for visualisation purposes.
+Columns were **renamed** to avoid unnecessary words and clarify the dataframe.
+
+After primary analyses, it standed out that data about only 5 cities were uploaded instead of the presumed 6 (see GitHub repo where the data were retrieved from).
+
+To remove duplicates we used the function ```distinct()``` from dplyr. 
+
+A **world map** was created to visualise the exact location of the cities and have a direct better understanding of next analyses.
+![world map](C:\Users\Hadrien Venance\python_and_r_luiss_2021\python_and_r_luiss_2021\Map.png)
+
+Then, this scatterplot seems to indicate that elevation is not really an indicator of the temperature.
+![temperature](C:\Users\Hadrien Venance\python_and_r_luiss_2021\python_and_r_luiss_2021\temp.png)
+
+It is further confirmed thanks to the next **facet visualisation**. 
+![facet](C:\Users\Hadrien Venance\python_and_r_luiss_2021\python_and_r_luiss_2021\facet.png)
+
+Unsurprisingly given the plots, the **correlation** between elevation and temperature is close to 0.
+![corr_elevation](C:\Users\Hadrien Venance\python_and_r_luiss_2021\python_and_r_luiss_2021\corr_elevation.png)
+
+To have a better visualization of which city had the highest temperature on each day, some manipulations have been made.
+Clearly, **Tokyo** has the highest temperature and **London** the coolest ones.
+![temp_cities](C:\Users\Hadrien Venance\python_and_r_luiss_2021\python_and_r_luiss_2021\temp_cities.png)
+
+In the database, there were 2 kind of temperatures, namely the **air temperature 2 meters above the ground** and the **perceived
+feel-like temperature**. 
+As can be seen from the next 3D plot it seems that a higher apparent temperature is as expected related with a higher real measured temperature.
+
+
+In ordrer to confirm these visual insights, let us compute the correlation.
+The **correlation matrix** (on the new subset of the dataframe _feels_temp_by_cities_) correlations are really high between temperature and feels like temperature, as well as between temperatures and humidity.
+![corr_temps](C:\Users\Hadrien Venance\python_and_r_luiss_2021\python_and_r_luiss_2021\corr_temps.png)
+
+
+
+Other variables were the **humidity** and the **precipitation**. 
+It seems that the **elevation** is slightly highly correlated with the humidity.
+Surprisingly, **precipitation** that happened the hour before the weather report does not seem correlated to **humidity** at all.
+![corr_water](C:\Users\Hadrien Venance\python_and_r_luiss_2021\python_and_r_luiss_2021\corr_water.png)
+
+**Atmospheric pressure** is an indicator of weather. As such, according to National Geographic,
+when a low-pressure system moves into an area, it usually leads to cloudiness, wind, and precipitation. High-pressure systems usually lead to fair, calm weather. 
+Let's try to verifiy this stated fact.
+
+![corr_atm](C:\Users\Hadrien Venance\python_and_r_luiss_2021\python_and_r_luiss_2021\corr_atm.png)
+
+
+Contrarily to what expected from national geographic, a higher **pressure** does not seem to indicate higher **precipitation**.
+
+In the end, the relation between the **freezing level height** and the **atmospheric pressure** was considered. 
+![pressure](C:\Users\Hadrien Venance\python_and_r_luiss_2021\python_and_r_luiss_2021\pressure.png)
+From the scatterplot, it seems that freezing height is not really related to pressure.
+
